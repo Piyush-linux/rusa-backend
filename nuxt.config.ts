218daf555f945e9ebca036d7bf1e5dedbbabcb47
Piyush-Linux <piyushpatil98@protonmail.com>
2024-01-05 14:52:28 +0530

    Deployed To Digital Ocean

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/strapi'],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    admin: '/admin',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt'
+  }
+})
